b2a0df881decdf79b039aec57a766017527f165c
MD. AL-AMIN TALUKDAR <timam@users.noreply.github.com>
2018-05-08 13:12:21 +0600

    added example server connection

--- Parse-Dashboard/index.js
+++ Parse-Dashboard/index.js
@@ -6,6 +6,36 @@
  * the root directory of this source tree.
  */
 // Command line tool for npm start
+
+var ParseDashboard = require('parse-dashboard');
+var ParseServer = require('parse-server').ParseServer;
+
+var dashboard = new ParseDashboard({
+    "apps": [{
+            "serverURL": "https://yourserver.url",
+            "appId": "myAppId",
+            "masterKey": "myMasterKey",
+            "appName": "YourAppName"
+        }
+    ],
+    "users": [{
+        "user": "user",
+        "pass": process.env.USER_PASS || "pass"
+    }]
+}, allowInsecureHTTP);
+
+var api = new ParseServer({
+    databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || 'myAppId',
+    masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+    liveQuery: {
+        classNames: ["Post", "Comments"] // List of classes to support for query subscriptions
+    }
+});
+
+
 "use strict"
 const path = require('path');
 const jsonFile = require('json-file-plus');
